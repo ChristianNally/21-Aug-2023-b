47e6ec8d045be3287b280e642a3837877a8cfb2c
Christian Nally <christian.nally@lighthouselabs.com>
2023-08-30 11:34:01 -0700

    in-class edits for chat server/client pair

--- w02d3_networking_with_tcp_and_http/client.js
+++ w02d3_networking_with_tcp_and_http/client.js
@@ -1,1 +1,25 @@
+const net = require('net');
+const port = 8005;
 
+const client = net.createConnection({
+    port: port,
+    host: 'localhost'
+});
+
+process.stdin.on('data', (message) => {
+    client.write(message);
+});
+
+client.setEncoding('utf8');
+
+client.on('connect', () => {
+    console.log('client is connected to the server.');
+});
+
+client.on('data', (message) => {
+    console.log(`${message}`);
+});
+
+client.on('end', () => {
+    console.log('client is disconnected from the server');
+});
--- w02d3_networking_with_tcp_and_http/server.js
+++ w02d3_networking_with_tcp_and_http/server.js
@@ -1,1 +1,38 @@
+const net = require('net');
+const port = 8005;
+
+const connectedClients = [];
+
+const server = net.createServer();
+
+server.on('connection', (client) => {
+    connectedClients.push(client);
+    client.setEncoding('utf8');
+    console.log(`client is connected`);
+    // console.log(`client=`, client);
+
+    client.write(`Welcome to my awesome server! ⛵`);
+
+    client.on('data', (message) => {
+        console.log('client says: ', message);
+
+        if (message.startsWith('setName ')){
+            let clientName = message.replace(/setName /, '');
+            clientName = clientName.replace(/^\s+|\s+$/g, '');
+            console.log('setting client name to=', clientName);
+            client.name = clientName;
+        }
+
+        connectedClients.forEach((eachClient) => {
+            const outGoingMessage = `${client.name} > ${message}`;
+            eachClient.write(outGoingMessage);
+        });
+
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Server is listening on port=${port}`);
+});
 
