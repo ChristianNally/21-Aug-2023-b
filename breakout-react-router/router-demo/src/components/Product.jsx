5d07ccf28a7cf3bd63fc775fb68db1452d412f5a
Andy Lindsay <andydlindsay@gmail.com>
2023-10-31 15:49:49 -0700

    breakout over

--- breakout-react-router/router-demo/src/components/Product.jsx
+++ breakout-react-router/router-demo/src/components/Product.jsx
@@ -1,7 +1,30 @@
+import {useParams, useNavigate, Navigate} from 'react-router-dom';
+import {useEffect} from 'react';
+
 const Product = () => {
+  const params = useParams();
+  const productId = params.productId; // req.params.productId
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/about');
+    }, 3000);
+  }, []);
+
+  if (1 == 1) {
+    return <Navigate to="/home" />;
+  }
+
+  // useEffect(() => {
+  //   fetch(`/api/products/${productId}`)
+  //     .then()
+  // }, [productId]);
+
   return (
     <div>
-      <h2>Product Detail Page</h2>
+      <h2>Product Detail Page #{productId}</h2>
     </div>
   );
 };
